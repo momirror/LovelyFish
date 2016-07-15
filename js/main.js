da1fac200740b1b42711d719febeb0528dfe214b
moshanpi <spencer.mo@intel.com>
2016-07-15 17:18:48 +0800

    add background

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
 /**
  * Created by moshanpi on 7/15/16.
  */
+
+var bgCanvas,animationCanvas,bgContext,animationContext;
+
+document.body.onload=function () {
+  init();
+};
+
+
+function init() {
+  bgCanvas = document.getElementById('bg-canvas');
+  animationCanvas = document.getElementById('animation-canvas');
+  bgContext = bgCanvas.getContext('2d');
+  animationContext = animationCanvas.getContext('2d');
+  
+  initBgCanvas();
+  initAnimationCanvas();
+}
+
+function initBgCanvas() {
+  drawBackgroundImage();
+}
+
+function  initAnimationCanvas() {
+
+}
+
+function drawBackgroundImage() {
+  var bgImg = new Image();
+  bgImg.src = 'src/background.jpg';
+  bgImg.onload = function () {
+    bgContext.drawImage(bgImg,0,0);
+  }
+}
